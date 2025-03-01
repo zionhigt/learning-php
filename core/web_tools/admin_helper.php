ec0555cc781dd143b62fb3d5667e650611759578
zionhigt <larcher501@gmail.com>
2025-03-01 12:31:15 +0100

    IMPROVE user checking

--- core/web_tools/admin_helper.php
+++ core/web_tools/admin_helper.php
@@ -1,7 +1,17 @@
 <?php
+
+require_once "./core/web_tools/sessions_helper.php";
+
 function isAdmin (): bool
 {
-    session_start();
-    $admin_id = 1;
-    return $_SESSION["connected"] && $_SESSION["user"]["role_id"] === $admin_id;
+    session();
+    include "./config.php";
+    if (!isset($app_config["app"])) {
+        error_log("'app' config key must be exists into config file; isAdmin cannot be verified");
+        require "./500.php";
+    }
+    $admin_id = $app_config["app"]["admin_id"];
+    return session_status() === PHP_SESSION_ACTIVE &&
+            isset($_SESSION["user"]) &&
+            $_SESSION["user"]["role_id"]["id"] === $admin_id;
 }
