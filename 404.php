1d855d5d82ea2102f3e4a3b9e0c385ec5797fbbf
zionhigt <larcher501@gmail.com>
2025-03-01 12:26:16 +0100

    Add some error content

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php http_response_code(404); ?>
+
+<div class="container">
+    <h1>404 : Not found Error</h1>
+</div>
+<?php die(); ?>
---
+++ 500.php
@@ -0,0 +1,5 @@
+<?php http_response_code(500); ?>
+<div class="container">
+    <h1>500 : Internal server error</h1>
+</div>
+<?php die(); ?>
