5a3c1e56c8c18729fb82778639d313d1ad61be90
zionhigt <larcher501@gmail.com>
2025-03-01 12:34:14 +0100

    SIMPLE router

--- index.php
+++ index.php
@@ -3,25 +3,23 @@
 require './logger/index.php';
 require './mock/data.php';
 require './layout/header.php';
-require './layout/body.php';
-require './core/models/book.php';
 
-$books = new Books();
+$uri = isset($_SERVER["PATH_INFO"]) ? $_SERVER["PATH_INFO"] : $_SERVER["SCRIPT_NAME"];
+switch ($uri) {
+    case "/index.php":
+    case "/books":
+        require "./pages/books.php";
+        break;
+    case "/login.php":
+    case "/login":
+        require "./pages/login.php";
+        break;
+    case "/add.php":
+    case "/add":
+        require "./pages/add.php";
+        break;
+    default:
+        require "./404.php";
+}
 
-body(
-    $_GET["view"] ?? array_key_first($menu_list),
-    $books->filter(
-        function (int $id, array $item): bool {
-            $filter = $_GET["filter_status"] ?? "*";
-
-            if (!in_array($filter, ["read", 'noread', "progress"])) {
-                $filter = "*";
-            }
-            if ($filter === "*") {
-                return true;
-            }
-            return $item["status"] === $filter;
-        }
-    )
-);
 require './layout/footer.php';
