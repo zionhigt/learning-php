77cf9c710c0db6f3ffbf850a2a1b1314a7fa3a05
zionhigt <larcher501@gmail.com>
2025-03-01 12:34:36 +0100

    UPDATE to use new available route

--- components/menu.php
+++ components/menu.php
@@ -8,7 +8,7 @@ function menu_item(string $name, string $class_icon, ?bool $active): string
         $class .= " active";
         $attributes = 'aria-current="page"';
     }
-    $url = '/index.php?' . http_build_query(array_merge((array)$_GET, ["view" => $name]));
+    $url = '/books?' . http_build_query(array_merge((array)$_GET, ["view" => $name]));
     return <<<HTML
     <li class="nav-item">
         <a class="$class" href="$url" $attributes>$capitalized_name
